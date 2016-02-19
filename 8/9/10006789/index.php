222f7b6aad7ef39f9d919d331f9f7ff3d673cfd2
Max Yeo <mzwyeo@gmail.com>
2016-02-19 16:24:08 -0500

    login and login admin check

--- 8/9/10006789/index.php
+++ 8/9/10006789/index.php
@@ -3,3 +3,46 @@
 // login form
 
 ?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width">
+
+    <title>Login | Inception</title>
+    <meta name="description" content="">
+    <meta name="author" content="Ethical Hacking | Group 3 - CN & Max">
+
+    <link rel="shortcut icon" href="">
+    <link rel="apple-touch-icon" href="">
+
+    <meta name="theme-color" content="">
+
+    <meta property="og:title" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content=""/>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div id="bg-filter">
+        <form method="post" action="home.php">
+        	<input type="text" name="username">
+        	<input type="password" name="password">
+        	<input type="submit">
+        </form>
+
+        <footer>
+        </footer>
+
+    </div>
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/style.js"></script>
+</body>
+</html>
